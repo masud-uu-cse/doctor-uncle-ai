9bfa93811a2695942d8dc83d6a7f49ef93fb63ce
Masud Rana <masud309060@gmail.com>
2026-07-30 00:23:58 +0600

    feat: redesign language toggle component with display text and custom styling while cleaning up route configuration

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/doctor-uncle" element={<ChatWithDoctorUncle />} />
             <Route path="/history" element={<HistoryPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
